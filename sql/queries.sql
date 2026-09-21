2520cf393f3cadd9cfa9ed91d4a44c84664e2bdf
Priyansh <saxenapriyansh95@gmail.com>
2026-09-21 13:16:06 +0530

    added the continuous dates for vix and gspc

--- sql/queries.sql
+++ sql/queries.sql
@@ -9,5 +9,31 @@ CREATE TABLE gspc AS SELECT * FROM read_auto_csv('...path/^GSPC.csv')
 -- checking for NULLs 
 SELECT * FROM merged WHERE channel_name IS NULL OR video_id IS NULL OR duration_seconds IS NULL or view_count IS NULL or like_count IS NULL OR comment_count IS NULL 
 
--- truncating time to YYYY-MM-DD format in main 
-SELECT DATE_TRUNC('week', block_time) AS week,
+-- checking for date lags in vix and gspc 
+SELECT 
+    date,
+    LAG(date) OVER (ORDER BY date),
+    date - LAG(date) OVER (ORDER BY date) AS day_gap
+FROM 
+    vix
+
+-- forward-filling the dates for gspc and vix 
+    -- creating a date spine 
+    CREATE TABLE date_spine AS 
+    SELECT CAST(generate_series AS date) AS continuous_date
+    FROM generate_series(
+        DATE '2023-01-01',
+        DATE '2026-09-15',
+        INTERVAL '1 DAY'
+    )
+
+    -- merging gspc and vix 
+    CREATE TABLE gspc_continuous_date AS 
+    SELECT * FROM date_spine 
+    LEFT JOIN gspc
+    ON date_spine.continuous_date = gspc.date
+
+    CREATE TABLE vix_continuous_date AS 
+    SELECT * FROM date_spine 
+    LEFT JOIN vix
+    ON date_spine.continuous_date = vix.date
