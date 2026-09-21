d0a640275d4764c0e7ef135400aaead19520de62
Priyansh <saxenapriyansh95@gmail.com>
2026-09-21 12:18:22 +0530

    updated the queries.sql script

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,8 +1,13 @@
 -- sql scripts used to conduct analysis on duckdb 
 
--- create table 
+-- create tables 
 CREATE TABLE main AS SELECT * FROM read_auto_csv('...path/youtube_videos.csv')
+CREATE TABLE vix AS SELECT * FROM read_auto_csv('...path/^VIX.csv')
+CREATE TABLE gspc AS SELECT * FROM read_auto_csv('...path/^GSPC.csv')
+
 
 -- checking for NULLs 
 SELECT * FROM merged WHERE channel_name IS NULL OR video_id IS NULL OR duration_seconds IS NULL or view_count IS NULL or like_count IS NULL OR comment_count IS NULL 
 
+-- truncating time to YYYY-MM-DD format in main 
+SELECT DATE_TRUNC('week', block_time) AS week,
