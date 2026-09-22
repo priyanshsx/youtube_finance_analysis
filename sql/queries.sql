68ae2b6555fbe86c4a21c462cf994850aec94846
Priyansh <saxenapriyansh95@gmail.com>
2026-09-22 10:26:57 +0530

    added the regex strings for description and title analysis

--- sql/queries.sql
+++ sql/queries.sql
@@ -53,11 +53,26 @@ SELECT
     ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS gspc_filled_close
 FROM gspc_continuous_date
 
+-- regex string for fear: 
+-- (?i)\b(crash|warning|collapse|bubble|crisis|recession|panic|emergency|disaster|doomsday|liquidation|debasement|screwed|trap|chaos|red flag|yikes|speechless|falter)\b|\b(it.?s over|do not buy|don.?t buy|lose everything|falls? apart|rushing for the exits?|much worse|end of the world|f\*\*k.?d)\b
 
+-- regex string for opportunity: 
+-- (?i)\b(skyrocket|bullish|moon|soar|banger|unbelievable)\b|\b(wealth transfer|generational|bull run|infinite money glitch|make millions|must buy|all in|life.?changing|never get a chance|filthy rich|go nuts|do not sell|don.?t sell|wealth rotation)\b
 
+-- regex string for evergreen: 
+-- (?i)\b(passive income|roth ira|401k|retire|budget|habits?|mindset|credit score|net worth|taxes|portfolio|salary|guide|milestones?)\b|\b(how to|explains?|buy.* vs .*rent|rent.* vs .*buy|financial plan|saving|debt mistakes?|wealth building|long game|getting rich|quiet quit)\b
 
--- title_theme: trading_ed, investing_ed, economic_ed, business_ed, ai_ed, tech_ed, life_ed,
--- is_sponsored: yes/no, 
--- sponsor_type (Brokerage, VPN, trading software, other), 
--- duration_bucket (short: <10, medium: 10-20, long: 20+)
+-- regex string for neutral: 
+-- (?i)\b(bitcoin|btc|crypto|altcoin|ai|dropshipping|real estate|mortgage|cpi|inflation|jobs report|fed|vix|gold|silver|wall street)\b
+
+-- regex string for is_sponsored 
+-- (?i)\b(sponsored by|thanks to.* for sponsoring|paid partnership|ad|)\b
+-- return TRUE/1 if any present or return FALSE/0 if none 
+
+-- Brokerages and trading apps: webull, robinhood, m1 finance, interactive brokers, moomoo, public.com
+-- Crypto and hardware wallets: coinbase, crypto.com, kraken, ledger, trezor, binance
+-- Tax, accounting and business services: turbotax, taxact, inc authority, rocket lawyer, quickbooks, 
+-- privacy and tech: nordvpn, expressvpn, surfshar, incogni, deleteme, audible 
+
+-- duration buckets: micro: < 180s; short: 181s - 600s, medium: 601s-1200s, long: 1201+
 
