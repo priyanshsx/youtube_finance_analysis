bcaf464fd10f90b7762d2989cb1637c5e4b06c4d
Priyansh <saxenapriyansh95@gmail.com>
2026-09-22 17:50:57 +0530

    updated the analysis script and readme

--- src/analysis.py
+++ src/analysis.py
@@ -11,7 +11,7 @@ con = duckdb.connect('/home/priyansh/Documents/d/youtube_finance_analysis/db/mai
 
 df = con.sql("SELECT * FROM master").df()
 
-# hypothesis testing: #1 (refer README)
+# answering question #1 (refer README)
 
 df_hyp1 = con.sql("""
     SELECT * FROM master WHERE title_category IN ('fear','evergreen')
@@ -19,16 +19,33 @@ df_hyp1 = con.sql("""
 
 df_hyp1['market_regime'] = np.where(df_hyp1['vix_close']  > 20, 'Volatile', 'Normal')
 
-filtered_df = df_hyp1.groupby(['market_regime', 'title_category'])['view_velocity'].median().unstack()
-# print(filtered_df)
+q1_df = df_hyp1.groupby(['market_regime', 'title_category'])['view_velocity'].median().unstack()
 
-# hypothesis testing: #3 (refer README)
+# answering question #2 (refer README)
 
 df['market_regime'] = np.where(df['vix_close'] > 20, 'Volatile', 'Normal')
 
-hyp3_vv = df.groupby(['market_regime', 'duration_bucket'])['view_velocity'].median().unstack()
-hyp3_er = df.groupby(['market_regime', 'duration_bucket'])['engagement_rate'].median().unstack()
-print("Hypothesis 3 - View Velocity:")
-print(hyp3_vv)
-print("\nHypothesis 3 - Engagement Rate:")
-print(hyp3_er)
+q2_vv = df.groupby(['market_regime', 'duration_bucket'])['view_velocity'].median().unstack()
+q2_er = df.groupby(['market_regime', 'duration_bucket'])['engagement_rate'].median().unstack()
+
+# answering question #7 (refer README)
+
+urgency_keywords = '(?i)\\b(warning|do this|emergency|before it.?s too late|alert|critical|now)\\b'
+df['urgency_keywords'] = np.where(df['title'].str.contains(urgency_keywords, regex=True, na=False), 'Urgent', 'Standard')
+
+q7_vv = df.groupby('urgency_level')['view_velocity'].median()
+q7_er = df.groupby('urgency_level')['engagement_rate'].median()
+
+# answering question #4 (refer README)
+
+
+print("\nPerformance Gap between Fear and Evergreen titles:")
+print(q1_df)
+print("Video Duration & View Velocity:")
+print(q2_vv)
+print("\nVideo Duration & Engagement Rate:")
+print(q2_er)
+print("\nMedian Views by Urgency Level:")
+print(q7_vv)
+print("\nEngagement Rate by Urgency Level:")
+print(q7_er)
