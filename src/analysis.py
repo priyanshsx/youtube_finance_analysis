c7eebb1e72d2d3a22ef549cfb2338c0f676dd970
Priyansh <saxenapriyansh95@gmail.com>
2026-09-22 12:56:04 +0530

    fired up the analysis script for this

--- src/analysis.py
+++ src/analysis.py
@@ -3,4 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from scipy import stats 
 import statsmodels.api as sm 
+import duckdb 
 
+con = duckdb.connect('/home/priyansh/Documents/d/youtube_finance_analysis/db/main.duckdb')
+
+df = con.sql("SELECT * FROM master").df()
+
+print(df.info())
