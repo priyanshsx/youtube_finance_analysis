6397f242b99baf8f341cba89aa2b0d599cdc9574
Priyansh <saxenapriyansh95@gmail.com>
2026-09-22 15:58:48 +0530

    added the final script for hypothesis 3

--- src/analysis.py
+++ src/analysis.py
@@ -20,4 +20,15 @@ df_hyp1 = con.sql("""
 df_hyp1['market_regime'] = np.where(df_hyp1['vix_close']  > 20, 'Volatile', 'Normal')
 
 filtered_df = df_hyp1.groupby(['market_regime', 'title_category'])['view_velocity'].median().unstack()
-print(filtered_df)
+# print(filtered_df)
+
+# hypothesis testing: #3 (refer README)
+
+df['market_regime'] = np.where(df['vix_close'] > 20, 'Volatile', 'Normal')
+
+hyp3_vv = df.groupby(['market_regime', 'duration_bucket'])['view_velocity'].median().unstack()
+hyp3_er = df.groupby(['market_regime', 'duration_bucket'])['engagement_rate'].median().unstack()
+print("Hypothesis 3 - View Velocity:")
+print(hyp3_vv)
+print("\nHypothesis 3 - Engagement Rate:")
+print(hyp3_er)
